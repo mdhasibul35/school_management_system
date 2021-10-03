af816549d6a3832299aff62d5c6d9b6b2bda0e96
hasib <hasibulhasan35.web@gmail.com>
2021-10-04 00:32:52 +0600

    **

---
+++ src/sms/Sms.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sms;
+
+/**
+ *
+ * @author Hasib
+ */
+/**
+ * school management system
+ * @author Hasib
+ */
+public class Sms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        open frame=new open();
+        frame.setVisible(true) ;
+    }
+    
+}
